d0bb585ce6967701de75fe70093f2ed69c578532
mirul49 <25042885@myrp.edu.sg>
2026-06-29 21:51:31 +0800

    Refactor main.py to improve structure and clarity

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
+# Import functions from settings.py
+from settings import show_difficulty_options, choose_difficulty, get_settings
+
+# Import functions from leaderboard.py
 from leaderboard import save_score, show_leaderboard
 
 
-
-
-
-#  AMIRUL'S PART
-# Ask the player to enter their username
-# The username will be saved together with the score
+# Ask player for username
 username = input("Enter your username: ")
 
 
-# Display a message to show that the game is starting
+# Show difficulty menu
+show_difficulty_options()
+
+
+# Ask player to choose difficulty
+choice = input("Choose difficulty 1, 2, or 3: ")
+
+
+# Convert the choice into Easy, Medium, or Hard
+difficulty = choose_difficulty(choice)
+
+
+# Get the settings for the selected difficulty
+game_settings = get_settings(difficulty)
+
+
+# Show selected game settings
+print("\n===== Selected Settings =====")
+print("Difficulty:", game_settings["difficulty"])
+print("Time Limit:", game_settings["time_limit"], "seconds")
+print("Word Speed:", game_settings["word_speed"])
+print("Word List:", game_settings["word_list"])
+
+
+# Start game message
 print("\nStarting TypeRush...")
 
-# Display a message to show that the game has ended
-print("Game ended!")
 
-
-# Ask the player to enter their score manually
-# Later, this line will be replaced by the actual score from game.py
+# Temporary score input for testing
+# Later, Cheng Rou can replace this with the real score from game.py
 score = int(input("Enter your score: "))
 
 
-# Save the username and score into data/scores.json
-# This comes from the save_score function inside leaderboard.py
+# Save username and score into data/scores.json
 save_score(username, score)
 
-# Display a message to confirm that the score has been saved
-print("\nYour score has been saved.")
 
+# Confirm score is saved
+print("\nYour score has been saved!")
+
+
+# Show leaderboard after saving score
 show_leaderboard()
