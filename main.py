668cb566251f9a779e652beb5c545fee79ab2737
mirul49 <25042885@myrp.edu.sg>
2026-06-29 21:48:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-import json
-import os
-
-SCORES_FILE = "data/scores.json"
-
-# AMIRUL'S PART
-def get_leaderboard():
-    scores = load_scores()
-
-    scores.sort(key=lambda x: x["score"], reverse=True)
-
-    return scores
+from leaderboard import save_score, show_leaderboard
 
 
-def show_leaderboard():
-    scores = get_leaderboard()
 
-    print("\n===== TypeRush Leaderboard =====")
 
-    if len(scores) == 0:
-        print("No scores yet.")
-    else:
-        rank = 1
 
-        for player in scores:
-            print(str(rank) + ". " + player["username"] + " - " + str(player["score"]))
-            rank += 1
+#  AMIRUL'S PART
+# Ask the player to enter their username
+# The username will be saved together with the score
+username = input("Enter your username: ")
+
+
+# Display a message to show that the game is starting
+print("\nStarting TypeRush...")
+
+# Display a message to show that the game has ended
+print("Game ended!")
+
+
+# Ask the player to enter their score manually
+# Later, this line will be replaced by the actual score from game.py
+score = int(input("Enter your score: "))
+
+
+# Save the username and score into data/scores.json
+# This comes from the save_score function inside leaderboard.py
+save_score(username, score)
+
+# Display a message to confirm that the score has been saved
+print("\nYour score has been saved.")
+
+show_leaderboard()
